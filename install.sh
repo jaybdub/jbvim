6b4fa6400098e0e1c5331db2b34a8f2939d6a6bb
John Welsh <jbmy13@gmail.com>
2017-11-23 04:16:01 -0500

    added stuff

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 cp .config/i3/config ~/.config/i3/config
 cp .vimrc ~/.vimrc
 cp .Xresources ~/.Xresources
-xrdb -merge ~/.Xresources
+#xrdb -merge ~/.Xresources
+xrdb ~/.Xresources
 i3-msg reload
 i3-msg restart
