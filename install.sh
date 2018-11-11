500729f2cb041bee1dcd86560aad9bccd5dac6b4
John Welsh <jwelsh@nvidia.com>
2018-11-11 19:41:18 +0000

    added flake8

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-sudo apt install cmake exuberant-ctags
+sudo apt install cmake exuberant-ctags flake8
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
