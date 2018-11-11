b5521c4299de28f3bf39aa6de0c96a28dedadde4
John Welsh <jwelsh@nvidia.com>
2018-11-11 19:50:03 +0000

    added google codefmt

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 sudo apt install cmake exuberant-ctags flake8
+pip install pep8 yapf --user
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp .vimrc ~/.vimrc
